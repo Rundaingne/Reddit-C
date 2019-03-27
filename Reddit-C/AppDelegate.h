96af9923a3395ed8e66a140ba8456de807e9331a
Erich Kumpunen <erichtheviolin@gmail.com>
2019-03-27 10:57:27 -0600

    Initial Commit

---
+++ Reddit-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Reddit-C
+//
+//  Created by Brooke Kumpunen on 3/27/19.
+//  Copyright © 2019 Rund LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
